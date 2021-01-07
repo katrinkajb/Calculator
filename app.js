13ea1ad64d7c549331ec0083f05e9397a566366c
katrinkajb@gmail.com <katrinkajb@gmail.com>
2021-01-06 16:02:46 -0800

    more buttons and actions

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const welcomeElement = document.getElementById('welcome');
 const welcomeButton = document.getElementById('change-button');
 const lowerButton = document.getElementById('lower-button');
 const lowerPara = document.getElementById('lower-para');
+const paraInput = document.getElementById('para-input');
+const middleButton = document.getElementById('middle-button')
+const upperPara = document.getElementById('upper-para');
 
 // initialize state
 
@@ -13,11 +16,19 @@ welcomeButton.addEventListener('click', () => {
     welcomeButton.textContent = 'How Dare You';
 })
 
+middleButton.addEventListener('click', () => {
+    upperPara.textContent = paraInput.value;
+    upperPara.style.color = 'green';
+})
+
+
 lowerButton.addEventListener('click', () => {
     console.log('clicked lower button');
     lowerButton.textContent = 'You Monster';
-    lowerPara.style.transform = 'rotate(30deg)';
+    lowerPara.style.transform = 'rotate(350deg)';
     lowerPara.textContent = "I can't believe you clicked! Now you've done it..."
+    lowerPara.style.color = 'red'
+
 })
 
 // PLAN. Add button, when user clicks button, change style of welcome element
